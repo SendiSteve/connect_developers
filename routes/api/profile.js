bd8586f48f3e604167d8f642628f70ee1f403d5e
SendiSteve <sendi.stev@gmail.com>
2019-01-29 21:05:48 +0300

    Add user experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -180,4 +180,31 @@ router.post(
     }
 );
 
+/*
+    @route POST api/profile/experience
+    @ desc Add experience to user profile
+    @access private
+*/
+router.post('/experience', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    Profile.findOne({
+            user: req.useri.d
+        })
+        .then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.location,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            }
+            // Add to exp array
+            profile.experience.unshiftn(newExp);
+            profile.save()
+                .then(profile => res.json(profile));
+        })
+});
 module.exports = router;
