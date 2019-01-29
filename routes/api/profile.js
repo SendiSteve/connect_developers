172cd693c95f62333b3cd52334f6b11b7ec94384
SendiSteve <sendi.stev@gmail.com>
2019-01-29 20:22:27 +0300

    Add route to search user by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,6 +58,26 @@ router.get('/handle/:handle', (req, res) => {
         .catch(err => res.status(404).json(err));
 });
 
+/*
+    @route GET api/profile/user/:user_id
+    @ desc get user profile by id
+    @access public
+*/
+router.get('/user/:user_id', (req, res) => {
+    const errors = {}
+    Profile.findOne({
+            handle: req.params.user_id
+        })
+        .populate('user', ['username', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profilef for this user';
+                res.status(404).json(errors)
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 /*
     @route POST api/profile
