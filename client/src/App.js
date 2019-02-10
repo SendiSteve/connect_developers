a59911c28fa2b09f63b9d17ae7a369f9fa74c45d
SendiSteve <sendi.stev@gmail.com>
2019-02-10 09:15:37 +0300

    Add landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-
+import Home from './components/layout/Home';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
+        <Home />
         <Footer />
       </div>
     );
---
+++ client/src/components/layout/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+        <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Connect Developers</h1>
+                <p className="lead">
+                  Easily connect with fellow developers, create a profile, share posts and get help from developers.
+                </p>
+                <hr />
+                <a href="/register" className="btn btn-lg btn-danger mr-2">
+                  Sign Up
+                </a>
+                <a href="/login" className="btn btn-lg btn-light">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Home;
