638d51f2d51afa7195b6aacbb2fb129836d689c9
SendiSteve <sendi.stev@gmail.com>
2019-02-19 08:53:00 +0300

    Configure Redux login Action an dset current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
-import { GET_ERRORS } from './types'
+import setAuthToken from '../utils/setAuthToken'
+import { GET_ERRORS, SET_CURRENT_USER } from './types'
+import jwt_decode from 'jwt-decode'
 
 
 // Register user
@@ -14,4 +16,38 @@ export const registerUser = (userData, history) => dispatch => {
                 payload: err.response.data
             })
             )
+}
+
+// login - Get user token
+export const loginUser = (userData) => dispatch => {
+    // make a post request
+    axios
+        .post('/api/users/login', userData)
+        .then(res => {
+            // Save to local storage
+            const {token} = res.data
+            // set token to local storage
+            localStorage.setItem('jwtToken', token)
+            // set token to auth header
+            setAuthToken(token)
+            // Decode token to get user data
+            const decoded = jwt_decode(token)
+            // set current user
+            dispatch(setCurrentUser(decoded))
+
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+// set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        patload: decoded
+    }
 }
